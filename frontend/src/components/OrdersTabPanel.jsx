8c0dd4b6920e1d03f16791e51ac5bbf08f4fbdcb
Sebastián Rivera <85461741+SebastRivera@users.noreply.github.com>
2023-08-28 02:17:43 +0000

    Detalles de p. y arreglo de tabla

--- frontend/src/components/OrdersTabPanel.jsx
+++ frontend/src/components/OrdersTabPanel.jsx
@@ -6,6 +6,15 @@ import useOrders from '../utils/hooks/useOrders'; // Importa el hook personaliza
 const OrdersTabPanel = ({ value, index }) => {
   const orders = useOrders(); // Utiliza el hook personalizado para obtener las órdenes
 
+  // Datos quemados para mostrar una fila
+  const dummyOrder = {
+    _id: '1',
+    product_id: '123',
+    quantity: 2,
+    nota: 'Sin cebolla',
+    total_pedido: '$20',
+  };
+
   return (
     <div
       role="tabpanel"
@@ -15,11 +24,10 @@ const OrdersTabPanel = ({ value, index }) => {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>Contenido de Ordenes</Typography>
           <TableContainer>
             <Table>
               <TableHead>
-                <TableRow>
+                <TableRow style={{ borderBottom: '3px solid #706E6E' }}>
                   <TableCell>Producto ID</TableCell>
                   <TableCell>Cantidad</TableCell>
                   <TableCell>Nota</TableCell>
@@ -28,6 +36,22 @@ const OrdersTabPanel = ({ value, index }) => {
                 </TableRow>
               </TableHead>
               <TableBody>
+                {/* Fila con datos quemados */}
+                <TableRow key={dummyOrder._id} >
+                  <TableCell>{dummyOrder.product_id}</TableCell>
+                  <TableCell>{dummyOrder.quantity}</TableCell>
+                  <TableCell>{dummyOrder.nota}</TableCell>
+                  <TableCell>{dummyOrder.total_pedido}</TableCell>
+                  <TableCell>
+                    <IconButton style={{ color: 'green' }}>
+                      <Check />
+                    </IconButton>
+                    <IconButton style={{ color: 'red' }}>
+                      <Clear />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+                {/* Renderiza las órdenes reales */}
                 {orders.map(order => (
                   <TableRow key={order._id}>
                     <TableCell>{order.product_id}</TableCell>
@@ -35,10 +59,10 @@ const OrdersTabPanel = ({ value, index }) => {
                     <TableCell>{order.nota}</TableCell>
                     <TableCell>{order.total_pedido}</TableCell>
                     <TableCell>
-                      <IconButton>
+                    <IconButton style={{ color: 'green' }}>
                         <Check />
                       </IconButton>
-                      <IconButton>
+                      <IconButton style={{ color: 'red' }}>
                         <Clear />
                       </IconButton>
                     </TableCell>
